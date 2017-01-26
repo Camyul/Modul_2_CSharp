774808b8740c0aa9d6c776b23e4c238f4f47ac1c
Camyul <vankata.lt@outlook.com>
2017-01-26 13:26:51 +0200

    Working on SHarp Task 2

--- CSharp - Unit Testing/02. Unit test the Deck class/ClassLibrary/Extensions/PlayerPositionExtensions.cs
+++ CSharp - Unit Testing/02. Unit test the Deck class/ClassLibrary/Extensions/PlayerPositionExtensions.cs
@@ -4,19 +4,19 @@
 
     public static class PlayerPositionExtensions
     {
-        public static PlayerPosition OtherPlayer(this PlayerPosition playerPosition)
-        {
-            switch (playerPosition)
-            {
-                case PlayerPosition.FirstPlayer:
-                    return PlayerPosition.SecondPlayer;
-                case PlayerPosition.SecondPlayer:
-                    return PlayerPosition.FirstPlayer;
-                case PlayerPosition.NoOne:
-                    return PlayerPosition.NoOne;
-                default:
-                    throw new ArgumentException("Invalid PlayerPosition value", nameof(playerPosition));
-            }
-        }
+        //public static PlayerPosition OtherPlayer(this PlayerPosition playerPosition)
+        //{
+        //    switch (playerPosition)
+        //    {
+        //        case PlayerPosition.FirstPlayer:
+        //            return PlayerPosition.SecondPlayer;
+        //        case PlayerPosition.SecondPlayer:
+        //            return PlayerPosition.FirstPlayer;
+        //        case PlayerPosition.NoOne:
+        //            return PlayerPosition.NoOne;
+        //        default:
+        //            throw new ArgumentException("Invalid PlayerPosition value", nameof(playerPosition));
+        //    }
+        //}
     }
 }
--- CSharp - Unit Testing/02. Unit test the Deck class/DeckTest/DeckTest.cs
+++ CSharp - Unit Testing/02. Unit test the Deck class/DeckTest/DeckTest.cs
@@ -2,12 +2,23 @@
 {
     using System;
     using NUnit.Framework;
+    using Santase.Logic.Cards;
     [TestFixture]
     public class DeckTest
     {
-        [Test]
-        public void TestMethod1()
+        [TestCase(1,23)]
+        [TestCase(5,19)]
+        [TestCase(24,0)]
+        public void GetNext_ShouldBeIncreaseDeckCount(int getCardNum, int result)
         {
+            IDeck testDeck = new Deck();
+
+            for (int i = 0; i < getCardNum; i++)
+            {
+                testDeck.GetNextCard();
+            }
+
+            Assert.AreEqual(testDeck.CardsLeft, result);
         }
     }
 }

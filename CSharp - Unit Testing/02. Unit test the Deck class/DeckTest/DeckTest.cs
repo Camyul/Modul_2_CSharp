e0f6585a1f990e5e4080e2879b734b0c4f431261
Camyul <vankata.lt@outlook.com>
2017-01-26 09:14:33 +0200

    Start working on SHarp Task 2

---
+++ CSharp - Unit Testing/02. Unit test the Deck class/DeckTest/DeckTest.cs
@@ -0,0 +1,13 @@
+﻿namespace DeckTest
+{
+    using System;
+    using NUnit.Framework;
+    [TestFixture]
+    public class DeckTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}

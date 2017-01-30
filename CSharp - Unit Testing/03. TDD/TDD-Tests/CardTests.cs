366de1571e1c295de382c5fe8248b1833779a122
Camyul <vankata.lt@outlook.com>
2017-01-30 20:52:34 +0200

    Work on Hand ToString test

---
+++ CSharp - Unit Testing/03. TDD/TDD-Tests/CardTests.cs
@@ -0,0 +1,38 @@
+﻿namespace TDD_Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Poker;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.Linq;
+    [TestClass]
+    public class CardTests
+    {
+        [TestMethod]
+        public void CardToString_ShouldBeCardFaceAndCardSuitToString()
+        {
+            ICard testCard = new Card(CardFace.Four, CardSuit.Diamonds);
+
+            string result = testCard.ToString();
+
+            Assert.AreEqual((CardFace.Four.ToString() + CardSuit.Diamonds.ToString()), result);
+        }
+
+        [TestMethod]
+        public void HandToStrind_ShouldBeCardsInHandToString()
+        {
+            IHand hand = new Hand ( new List<ICard> {
+                new Card(CardFace.Four, CardSuit.Diamonds ),
+                new Card(CardFace.Four, CardSuit.Clubs ),
+                new Card(CardFace.Four, CardSuit.Hearts ),
+                new Card(CardFace.Four, CardSuit.Spades ),
+                new Card(CardFace.Four, CardSuit.Diamonds )
+            } );
+
+            string handToStr = String.Join("\n", hand);
+
+            Assert.AreEqual(hand.ToString(), handToStr);
+        } 
+    }
+}

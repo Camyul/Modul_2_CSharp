70642a10a686ee5c03e1b4d3a3cf5ce5ae9c37d0
Camyul <vankata.lt@outlook.com>
2017-02-10 01:21:23 +0200

    Workshop Add Test for internal Class

---
+++ CSharp - Unit Testing/WorkShop - Academy/Academy.Tests/Commands/Adding/AddStudentsToSeasonCommandsTest.cs
@@ -0,0 +1,75 @@
+﻿namespace Academy.Tests.Commands.Adding
+{
+    using Academy.Commands.Adding;
+    using Academy.Core;
+    using Academy.Core.Contracts;
+    using Academy.Core.Factories;
+    using NUnit.Framework;
+    using System;
+    using System.Reflection;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Academy.Models.Contracts;
+    using Academy.Models;
+    using Academy.Models.Enums;
+    using System.Collections.Generic;
+    using System.Collections;
+    [TestFixture]
+    class AddStudentsToSeasonCommandsTest
+    {
+
+
+        /*Internal classes need to be tested and there is an assemby attribute:
+
+            using System.Runtime.CompilerServices;
+            [assembly: InternalsVisibleTo("NameOfYourUnitTestProject")]
+
+        Add this to the project info file, e.g.Properties\AssemblyInfo.cs.*/
+
+        [Test]
+        public void Ctor_ShouldThrowArgumentNullException_WhenPassedNullValue()
+        {
+            IAcademyFactory testFactory = AcademyFactory.Instance;
+            IEngine testEngine = Engine.Instance;
+            var testCommand = new AddStudentToSeasonCommand(testFactory, testEngine);
+
+            Assert.Throws<ArgumentNullException>(() => new AddStudentToSeasonCommand(null, testEngine));
+        }
+
+        //[Test]
+        //public void Ctor_ShouldAssignFactory_WhenPassedValueIsValid()
+        //{
+        //    IAcademyFactory testFactory = AcademyFactory.Instance;
+        //    IEngine testEngine = Engine.Instance;
+        //    var testCommand = new AddStudentToSeasonCommand(testFactory, testEngine);
+
+        //    var result = RunStatic(typeof(AddStudentToSeasonCommand), "factory");
+
+        //    Assert.AreEqual(testFactory, result);
+        //}
+
+        //private static object RunStatic(Type classType, string method)
+        //{
+        //    var methodInfo = classType.GetMethod(method,
+        //    BindingFlags.NonPublic | BindingFlags.Static);
+        //    return methodInfo.Invoke(null, null);
+        //}
+
+        //[Test]
+        //public void Execute_ShouldThrowArgumentException_WhenPassedStudentIsAlredyPartOfSeason()
+        //{
+        //    //Arrange
+        //    IAcademyFactory testFactory = AcademyFactory.Instance;
+        //    IEngine testEngine = Engine.Instance;
+        //    var testCommand = new AddStudentToSeasonCommand(testFactory, testEngine);
+        //    var testNewStudent = new List<string>{ "Ivanov", "01" };
+
+        //    //Act 
+        //    testCommand.Execute(testNewStudent);
+
+        //    //Assert
+        //    Assert.Throws<ArgumentException>(() => testCommand.Execute(testNewStudent));
+        //}
+    }
+}
---
+++ CSharp - Unit Testing/WorkShop - Academy/Academy.Tests/Core/Factories/AcademyFactoryTest.cs
@@ -0,0 +1,69 @@
+﻿namespace Academy.Tests.Core.Factories
+{
+    using Academy.Core.Contracts;
+    using Academy.Core.Factories;
+    using Academy.Models.Contracts;
+    using Academy.Models.Utils.LectureResources;
+    using NUnit.Framework;
+    using System;
+    [TestFixture]
+    class AcademyFactoryTest
+    {
+        [Test]
+        public void CreateLectureResourse_ShouldThrowArgumentException_WhenPassedTypeIsInvalid()
+        {
+            //Arrange
+            IAcademyFactory testFactory = AcademyFactory.Instance;
+
+            //Act & Assert
+
+            Assert.Throws<ArgumentException>(() => testFactory.CreateLectureResource("clip", "CSS", "http://telerikacademy.com"));
+
+
+        }
+
+        [Test]
+        public void CreateLectureResourse_ShouldAssignCorrectData_WhenPassedTypeVideo()
+        {
+            //Arrange
+            IAcademyFactory testFactory = AcademyFactory.Instance;
+
+            //Act & Assert
+
+            Assert.IsInstanceOf<VideoResource>(testFactory.CreateLectureResource("video", "CSS", "http://telerikacademy.com"));
+        }
+
+        [Test]
+        public void CreateLectureResourse_ShouldAssignCorrectData_WhenPassedTypePresentation()
+        {
+            //Arrange
+            IAcademyFactory testFactory = AcademyFactory.Instance;
+
+            //Act & Assert
+
+            Assert.IsInstanceOf<PresentationResource>(testFactory.CreateLectureResource("presentation", "CSS", "http://telerikacademy.com"));
+        }
+
+        [Test]
+        public void CreateLectureResourse_ShouldAssignCorrectData_WhenPassedTypeDemo()
+        {
+            //Arrange
+            IAcademyFactory testFactory = AcademyFactory.Instance;
+
+            //Act & Assert
+
+            Assert.IsInstanceOf<DemoResource>(testFactory.CreateLectureResource("demo", "CSS", "http://telerikacademy.com"));
+        }
+
+        [Test]
+        public void CreateLectureResourse_ShouldAssignCorrectData_WhenPassedTypeHomework()
+        {
+            //Arrange
+            IAcademyFactory testFactory = AcademyFactory.Instance;
+
+            //Act & Assert
+
+            Assert.IsInstanceOf<HomeworkResource>(testFactory.CreateLectureResource("homework", "CSS", "http://telerikacademy.com"));
+        }
+    }
+}

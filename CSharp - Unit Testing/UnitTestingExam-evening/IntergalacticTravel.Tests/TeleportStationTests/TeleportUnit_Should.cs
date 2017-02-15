54bffc8df703a7e898d39da497bd3293e5357225
Camyul <vankata.lt@outlook.com>
2017-02-15 11:29:20 +0200

    TestExam Add Two Tests for TeleportUnit

---
+++ CSharp - Unit Testing/UnitTestingExam-evening/IntergalacticTravel.Tests/TeleportStationTests/TeleportUnit_Should.cs
@@ -0,0 +1,49 @@
+﻿using IntergalacticTravel.Contracts;
+using IntergalacticTravel.Tests.Mock;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergalacticTravel.Tests.TeleportStationTests
+{
+    [TestFixture]
+    public class TeleportUnit_Should
+    {
+        [Test]
+        public void TeleportUnitThrowArgumentNullException_WhenUnitToTeleportIsNull()
+        {
+            //Arrange
+            var targetLocation = new Mock<ILocation>();
+            //var teleportStationMock = new TeleportStationMock(ownerStub.Object, galacticMapStub.Object, locationStub.Object);targetLocation.SetupGet(x => x.Planet.Name).Returns("Protos");
+
+            var ownerStub = new Mock<IBusinessOwner>();
+            var galacticMapStub = new Mock<IEnumerable<IPath>>();
+            var locationStub = new Mock<ILocation>();
+
+            var teleportStationMock = new TeleportStationMock(ownerStub.Object, galacticMapStub.Object, locationStub.Object);
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => teleportStationMock.TeleportUnit(null, targetLocation.Object));
+        }
+
+        [Test]
+        public void TeleportUnitThrowArgumentNullException_WhenDestinationIsNull()
+        {
+            //Arrange
+            var unitToTeleport = new Unit(123, "BattleShip Galaktica");
+
+            var ownerStub = new Mock<IBusinessOwner>();
+            var galacticMapStub = new Mock<IEnumerable<IPath>>();
+            var locationStub = new Mock<ILocation>();
+
+            var teleportStationMock = new TeleportStationMock(ownerStub.Object, galacticMapStub.Object, locationStub.Object);
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => teleportStationMock.TeleportUnit(unitToTeleport, null));
+        }
+    }
+}

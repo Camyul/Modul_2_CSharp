f09b731623097c1cf16662b60ff88d7a695ad33a
Camyul <vankata.lt@outlook.com>
2017-03-20 13:44:55 +0200

    CS Variable-Expession-Const Task-2 Complete

---
+++ High-Quality-Code-Part-1/05. Variables-Data-Expressions-Const/02. Method PrintStatistics in CSharp/Statistic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_PrintStatistics_in_CSharp
+{
+    class Statistic
+    {
+        
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double max = double.MinValue;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            PrintMax(max);
+            
+            double min = double.MaxValue;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            PrintMin(min);
+
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += arr[i];
+            }
+            PrintAvg(sum / count);
+        }
+
+        private void PrintAvg(double AverageSum)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PrintMin(double min)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PrintMax(double max)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+

933b57e447487a23c323c0d5cc0e24477ce3f2a7
Camyul <vankata.lt@outlook.com>
2017-03-21 02:07:44 +0200

    CS Control Flow Conditions Loop Task-2 Start work

---
+++ High-Quality-Code-Part-1/06. Control Flow Conditions Loops/02. Refactor the following if statements/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Refactor_the_following_if_statements
+{
+    class Program
+    {
+        }
+
+        Potato potato = new Potato();
+
+     if (potato != null)
+        {
+         if (potato.HasBeenPeeled && !potato.IsRotten)
+            {
+                Cook(potato);
+            }
+        }
+    }
+}
+    
+
---
+++ High-Quality-Code-Part-1/06. Control Flow Conditions Loops/02. Refactor the following if statements/potato.cs
@@ -0,0 +1,15 @@
+﻿namespace Refactor_the_following_if_statements
+{
+    public class Potato
+    {
+        public Potato()
+        {
+            this.HasBeenPeeled = true;
+            this.IsRotten = false;
+        }
+
+        public bool HasBeenPeeled { get; set; }
+
+        public bool IsRotten { get; set; }
+    }
+}

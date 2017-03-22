3b4d4bc42b8d57a6358dbc9cb5aed0a2b00c5467
Camyul <vankata.lt@outlook.com>
2017-03-22 14:44:27 +0200

    CS High-quality-Methods Task-1 Refactor Student class

---
+++ High-Quality-Code-Part-1/07. High-quality-Methods/Methods/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Student
+    {
+        private string firstName;
+
+        private string lastName;
+
+        private string otherInfo;
+
+        public Student(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("String cannot be null or empty");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("String cannot be null or empty");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            DateTime firstDate = GetDate(this.OtherInfo);
+               
+            DateTime secondDate = GetDate(other.OtherInfo);
+
+            bool isOlder = true;
+            if (firstDate < secondDate)
+            {
+                isOlder = false;
+            }
+            return isOlder;
+        }
+
+        private DateTime GetDate(string otherInfo)
+        {
+         DateTime date =  DateTime.Parse(otherInfo.Substring(otherInfo.Length - 10));
+
+            return date;
+        }
+    }
+}

6fab1c2de5318e9a4838c0573f827d523aaed745
Camyul <vankata.lt@outlook.com>
2017-03-23 11:27:18 +0200

    CS High-quality-Classes Task-3 Complete

--- High-Quality-Code-Part-1/08. High-quality-Classes/Cohesion-and-Coupling/Utils.cs
+++ High-Quality-Code-Part-1/08. High-quality-Classes/Cohesion-and-Coupling/Utils.cs
@@ -2,45 +2,69 @@
 
 namespace CohesionAndCoupling
 {
-    static class Utils
+    public static class Utils
     {
-        public static double Width { get; set; }
-        public static double Height { get; set; }
-        public static double Depth { get; set; }
+        private static double width;
+        private static double height;
+        private static double depth;
 
-        public static string GetFileExtension(string fileName)
+        public static double Width
         {
-            int indexOfLastDot = fileName.LastIndexOf(".");
-            if (indexOfLastDot == -1)
+            get
             {
-                return "";
+                return width;
             }
 
-            string extension = fileName.Substring(indexOfLastDot + 1);
-            return extension;
+            set
+            {
+                if (value > 0)
+                {
+                    width = value;
+                }
+            }
         }
 
-        public static string GetFileNameWithoutExtension(string fileName)
+        public static double Height
         {
-            int indexOfLastDot = fileName.LastIndexOf(".");
-            if (indexOfLastDot == -1)
+            get
             {
-                return fileName;
+                return height;
             }
 
-            string extension = fileName.Substring(0, indexOfLastDot);
-            return extension;
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public static double Depth
+        {
+            get
+            {
+                return depth;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                   depth = value;
+                }
+            }
         }
 
         public static double CalcDistance2D(double x1, double y1, double x2, double y2)
         {
-            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
             return distance;
         }
 
         public static double CalcDistance3D(double x1, double y1, double z1, double x2, double y2, double z2)
         {
-            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+            double distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)) + ((z2 - z1) * (z2 - z1)));
             return distance;
         }
 
--- High-Quality-Code-Part-1/08. High-quality-Classes/Cohesion-and-Coupling/UtilsExamples.cs
+++ High-Quality-Code-Part-1/08. High-quality-Classes/Cohesion-and-Coupling/UtilsExamples.cs
@@ -2,22 +2,20 @@
 
 namespace CohesionAndCoupling
 {
-    class UtilsExamples
+    internal class UtilsExamples
     {
-        static void Main()
+        internal static void Main()
         {
-            Console.WriteLine(Utils.GetFileExtension("example"));
-            Console.WriteLine(Utils.GetFileExtension("example.pdf"));
-            Console.WriteLine(Utils.GetFileExtension("example.new.pdf"));
+            Console.WriteLine(FileUtils.GetFileExtension("example"));
+            Console.WriteLine(FileUtils.GetFileExtension("example.pdf"));
+            Console.WriteLine(FileUtils.GetFileExtension("example.new.pdf"));
 
-            Console.WriteLine(Utils.GetFileNameWithoutExtension("example"));
-            Console.WriteLine(Utils.GetFileNameWithoutExtension("example.pdf"));
-            Console.WriteLine(Utils.GetFileNameWithoutExtension("example.new.pdf"));
+            Console.WriteLine(FileUtils.GetFileNameWithoutExtension("example"));
+            Console.WriteLine(FileUtils.GetFileNameWithoutExtension("example.pdf"));
+            Console.WriteLine(FileUtils.GetFileNameWithoutExtension("example.new.pdf"));
 
-            Console.WriteLine("Distance in the 2D space = {0:f2}",
-                Utils.CalcDistance2D(1, -2, 3, 4));
-            Console.WriteLine("Distance in the 3D space = {0:f2}",
-                Utils.CalcDistance3D(5, 2, -1, 3, -6, 4));
+            Console.WriteLine("Distance in the 2D space = {0:f2}", Utils.CalcDistance2D(1, -2, 3, 4));
+            Console.WriteLine("Distance in the 3D space = {0:f2}", Utils.CalcDistance3D(5, 2, -1, 3, -6, 4));
 
             Utils.Width = 3;
             Utils.Height = 4;

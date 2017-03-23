2542054d00a2d30ff0eae223ae31be5438fc645b
Camyul <vankata.lt@outlook.com>
2017-03-23 10:43:30 +0200

    CS High-quality-Classes Task-1 and Task-2 Complete

---
+++ High-Quality-Code-Part-1/08. High-quality-Classes/Abstraction/Figure.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Abstraction
+{
+    public abstract class Figure
+    {
+        private double width;
+        private double height;
+        private double radius;
+
+        public Figure()
+        {
+        }
+
+        public Figure(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public Figure(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Width cannot be negative!");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Height cannot be negative!");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Radius cannot be negative!");
+                }
+
+                this.radius = value;
+            }
+        }
+
+        public abstract double CalcPerimeter();
+
+        public abstract double CalcSurface();
+    }
+}

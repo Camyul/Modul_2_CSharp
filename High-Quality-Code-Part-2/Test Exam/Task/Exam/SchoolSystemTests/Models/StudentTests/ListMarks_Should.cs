f49835343a27a6bff3caff7fcd1020835ab27267
Camyul <vankata.lt@outlook.com>
2017-05-03 22:45:30 +0300

    CSharp Test Exam Add last unit test

--- High-Quality-Code-Part-2/Test Exam/Task/Exam/SchoolSystemTests/Models/StudentTests/ListMarks_Should.cs
+++ High-Quality-Code-Part-2/Test Exam/Task/Exam/SchoolSystemTests/Models/StudentTests/ListMarks_Should.cs
@@ -1,6 +1,9 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using SchoolSystem.Enum;
+using SchoolSystem.Enums;
 using SchoolSystem.Models;
+using SchoolSystem.Models.Contracts;
 using System;
 
 namespace SchoolSystemTests.Models.StudentTests
@@ -16,6 +19,25 @@ namespace SchoolSystemTests.Models.StudentTests
             var executionResult = student.ListMarks();
 
             StringAssert.Contains("no marks", executionResult);
+        }
+
+        [Test]
+        public void ShouldListMarks_WhenStudentHasMarks()
+        {
+            var markMock = new Mock<IMarks>();
+            markMock.SetupGet(x => x.Subject).Returns(Subject.English);
+            markMock.SetupGet(x => x.Valuation).Returns(4);
+
+            var student = new Student("Pesho", "Peshov", Grade.Eleventh);
+            //student.Marks.Add(markMock.Object); //Don't work, i don't know what
+
+            var executionResult = student.ListMarks()
+                .ToLower();
+
+            Assert.That(executionResult.Contains("has"));
+            Assert.That(executionResult.Contains("marks"));
+            Assert.That(executionResult.Contains("english"));
+            Assert.That(executionResult.Contains("4"));
         }
     }
 }

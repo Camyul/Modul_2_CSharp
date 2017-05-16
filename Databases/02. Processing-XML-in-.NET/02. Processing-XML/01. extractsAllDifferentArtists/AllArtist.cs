5f2b9b9bf929119fbde739b1f702cffbd6ffce9a
Camyul <vankata.lt@outlook.com>
2017-05-17 01:56:58 +0300

    DataBases - XML Processing - Extract all Artist

--- Databases/02. Processing-XML-in-.NET/02. Processing-XML/01. extractsAllDifferentArtists/AllArtist.cs
+++ Databases/02. Processing-XML-in-.NET/02. Processing-XML/01. extractsAllDifferentArtists/AllArtist.cs
@@ -16,7 +16,30 @@ namespace ExtractsAllDifferentArtists
             doc.Load("../../../catalogue.xml");
 
             XmlNode rootNode = doc.DocumentElement;
-            Console.WriteLine("Root Node {0}", rootNode.Name);
+           // Console.WriteLine("Root Node {0}", rootNode.Name);
+
+            var listOfArtists = new HashSet<string>();
+
+            foreach (XmlElement item in rootNode.ChildNodes)
+            {
+                var artist = item["artist"];
+               
+                listOfArtists.Add(artist.InnerText);
+            }
+            foreach (var name in listOfArtists)
+            {
+                int count = 0;
+                foreach (XmlElement item in rootNode.ChildNodes)
+                {
+                    var artist = item["artist"];
+
+                    if (name == artist.InnerText)
+                    {
+                        count++;
+                    } 
+                }
+                Console.WriteLine("{0} have {1} albums", name, count);
+            }
         }
     }
 }

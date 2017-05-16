acef0c3b1767a1c308d71c8f3ed378bd9d805a62
Camyul <vankata.lt@outlook.com>
2017-05-16 13:38:48 +0300

    Databases DOM Parser

---
+++ Databases/02. Processing-XML-in-.NET/02. Processing-XML/01. extractsAllDifferentArtists/AllArtist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ExtractsAllDifferentArtists
+{
+    class AllArstist
+    {
+        static void Main()
+        {
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("../../../catalogue.xml");
+
+            XmlNode rootNode = doc.DocumentElement;
+            Console.WriteLine("Root Node {0}", rootNode.Name);
+        }
+    }
+}

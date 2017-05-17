bc52e1d91cb7ab34e8a886c662c3f37cf8b2ed6a
Camyul <vankata.lt@outlook.com>
2017-05-17 16:09:56 +0300

    DataBases - JSON Processing - download xml file

---
+++ Databases/03. Processing-JSON-in-.NET/Processing JSON/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Processing_JSON
+{
+    class Program
+    {
+        static void Main()
+        {
+            WebClient client = new WebClient();
+
+            string adress = "https://www.youtube.com/feeds/videos.xml?channel_id=UCLC-vbm7OWvpbqzXaoAMGGw";
+
+            string pathToSave = "D:\\Programming\\Modul_2\\Modul_2_CSharp\\Databases\\03. Processing-JSON-in-.NET";
+
+            string fileName = pathToSave + "\\videos.xml";
+
+            client.DownloadFile(adress, fileName);
+        }
+    }
+}

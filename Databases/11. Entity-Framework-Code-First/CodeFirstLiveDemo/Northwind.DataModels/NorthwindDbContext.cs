fa905cb2695aef2a07eaacce0dde8b764e31caea
Camyul <vankata.lt@outlook.com>
2017-05-26 14:55:01 +0300

    Databases EF-CodeFirst Demo

---
+++ Databases/11. Entity-Framework-Code-First/CodeFirstLiveDemo/Northwind.DataModels/NorthwindDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Northwind.Data.Models;
+using System.Data.Entity;
+
+namespace Northwind.DataModels
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public NorthwindDbContext() : base("NorthwindDb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //прави връзка - много към много 
+            modelBuilder.Entity<City>().HasKey(x => x.Id).HasMany(x => x.Teachers).WithRequired(x => x.City);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public virtual IDbSet<Teacher> Teachers { get; set; }
+
+        public virtual IDbSet<City> Cities { get; set; }
+
+        public virtual IDbSet<Country> Countries { get; set; }
+    }
+}

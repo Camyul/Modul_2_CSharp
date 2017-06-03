e1c2597d9f8d4b0a8e4942a0c89bca049f0862b3
Camyul <vankata.lt@outlook.com>
2017-06-04 00:41:58 +0300

    Databases Workshop add two migrations

---
+++ Databases/Workshop/Code First/MostCompImport/MostDatabase.Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace MostDatabase.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Databases/Workshop/Code First/MostCompImport/MostDatabase/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using MostDatabase.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostDatabase
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MostDbContext, Configuration>());
+        }
+    }
+}

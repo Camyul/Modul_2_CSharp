1a0423adcce93223da669e561ceff35621f18400
Camyul <vankata.lt@outlook.com>
2017-06-07 11:07:48 +0300

    DP Add Homework-DealerShip

---
+++ Design-Patterns/Dealership-AuthorSolution/Dealership/DealershipModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dealership.Engine;
+using Dealership.Factories;
+using Ninject.Modules;
+
+namespace Dealership
+{
+    public class DealershipModule : NinjectModule
+    {
+        private const string UserNotLoggedCommandHandlerName = "UserNotLoggedCommandHandlerName";
+        private const string ShowVehiclesCommandHandlerName = "ShowVehiclesCommandHandlerName";
+        private const string ShowUsersCommandHandlerName = "ShowUsersCommandHandlerName";
+        private const string RemoveVehicleCommandHandlerName = "RemoveVehicleCommandHandlerName";
+        private const string RemoveCommentCommandHandlerName = "RemoveCommentCommandHandlerName";
+        private const string RegisterUserCommandHandlerName = "RegisterUserCommandHandlerName";
+        private const string LogoutCommandHandlerName = "LogoutCommandHandlerName";
+        private const string LoginCommandHandlerName = "LoginCommandHandlerName";
+        private const string AddVehicleCommandHandlerName = "AddVehicleCommandHandlerName";
+        private const string AddCommentCommandHandlerName = "AddCommentCommandHandlerName";
+
+        public override void Load()
+        {
+
+
+            Bind<IDealershipFactory>().To<DealershipFactory>().InSingletonScope();
+            Bind<IEngine>().To<DealershipEngine>().InSingletonScope();
+        }
+    }
+}
---
+++ Design-Patterns/Dealership-AuthorSolution/Dealership/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Dealership.Engine;
+using System;
+using System.IO;
+using Ninject;
+
+namespace Dealership
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            IKernel kernel = new StandardKernel(new DealershipModule());
+
+            IEngine engine = kernel.Get<IEngine>();
+            engine.Start();
+        }
+    }
+}

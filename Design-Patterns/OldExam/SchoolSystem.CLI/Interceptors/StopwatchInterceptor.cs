4ceacbe0d622915fde6c7238aa1ed8c7815be737
Camyul <vankata.lt@outlook.com>
2017-06-10 16:13:30 +0300

    DP OldExam Add Stopwatch Interceptor

---
+++ Design-Patterns/OldExam/SchoolSystem.CLI/Interceptors/StopwatchInterceptor.cs
@@ -0,0 +1,30 @@
+﻿using SchoolSystem.Framework.Core.Contracts;
+using System.Diagnostics;
+using Ninject.Extensions.Interception;
+
+namespace SchoolSystem.Cli.Interceptors
+{
+    public class StopwatchInterceptor : IInterceptor
+    {
+        private readonly IWriter writer;
+
+        private readonly Stopwatch stopwatch;
+
+        public StopwatchInterceptor(IWriter writer)
+        {
+            this.writer = writer;
+            this.stopwatch = new Stopwatch();
+        }
+        public void Intercept(IInvocation invocation)
+        {
+            this.writer.WriteLine($"Calling method {invocation.Request.Method.Name} of type {invocation.Request.Method.DeclaringType.Name}...");
+
+            stopwatch.Start();
+            invocation.Proceed();
+            stopwatch.Stop();
+
+            this.writer.WriteLine($"Total execution time for method {invocation.Request.Method.Name} of type {invocation.Request.Method.DeclaringType.Name} is {this.stopwatch.ElapsedMilliseconds} milliseconds.");
+
+        }
+    }
+}
--- Design-Patterns/OldExam/SchoolSystem.CLI/SchoolSystemModule.cs
+++ Design-Patterns/OldExam/SchoolSystem.CLI/SchoolSystemModule.cs
@@ -1,8 +1,10 @@
 ﻿using Ninject;
 using Ninject.Extensions.Conventions;
 using Ninject.Extensions.Factory;
+using Ninject.Extensions.Interception.Infrastructure.Language;
 using Ninject.Modules;
 using SchoolSystem.Cli.Configuration;
+using SchoolSystem.Cli.Interceptors;
 using SchoolSystem.Framework.Core;
 using SchoolSystem.Framework.Core.Commands;
 using SchoolSystem.Framework.Core.Commands.Contracts;
@@ -21,41 +23,56 @@ namespace SchoolSystem.Cli
     {
         public override void Load()
         {
+            //Биндва автоматично класовете и интерфейсите с еднакви имена, като тези доло
             Kernel.Bind(x =>
             {
                 x.FromAssembliesInPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
                 .SelectAllClasses()
                 .BindDefaultInterface();
             });
+            //Kernel.Bind<IStudent>().To<Student>();
+            //Kernel.Bind<ITeacher>().To<Teacher>();
+            //Kernel.Bind<IMark>().To<Mark>();
 
             Kernel.Bind<Engine>().ToSelf();
 
             Kernel.Bind<IReader>().To<ConsoleReaderProvider>();
             Kernel.Bind<IWriter>().To<ConsoleWriterProvider>();
             Kernel.Bind<IParser>().To<CommandParserProvider>();
-            //Kernel.Bind<IStudent>().To<Student>();
 
-            Kernel.Bind<CreateStudentCommand>().ToSelf().InSingletonScope();
-            Kernel.Bind<CreateTeacherCommand>().ToSelf().InSingletonScope();
+            Bind<CreateStudentCommand>().ToSelf().InSingletonScope();
+            Bind<CreateTeacherCommand>().ToSelf().InSingletonScope();
+            Bind<RemoveStudentCommand>().ToSelf().InSingletonScope();
+            Bind<RemoveTeacherCommand>().ToSelf().InSingletonScope();
+            Bind<StudentListMarksCommand>().ToSelf().InSingletonScope();
+            Bind<TeacherAddMarkCommand>().ToSelf().InSingletonScope();
 
-            Kernel.Bind<ICommandFactory>().ToFactory().InSingletonScope();
-            Kernel.Bind<IStudentFactory>().ToFactory().InSingletonScope();
-            Kernel.Bind<IMarkFactory>().ToFactory().InSingletonScope();
-            Bind<ITeacherFactory>().ToFactory().InSingletonScope();
+            var commandFactoryBinding = Kernel.Bind<ICommandFactory>().ToFactory().InSingletonScope();
+            var studentFactoryBinding = Kernel.Bind<IStudentFactory>().ToFactory().InSingletonScope();
+            var markFactoryBinding = Kernel.Bind<IMarkFactory>().ToFactory().InSingletonScope();
+            var teacherFactoryBinding = Bind<ITeacherFactory>().ToFactory().InSingletonScope();
 
             Bind(typeof(IAddStudent), typeof(IAddTeacher), typeof(IRemoveStudent), typeof(IRemoveTeacher), typeof(IGetStudent), typeof(IGetTeacher), typeof(IGetStudentAndTeacher))
                 .To<School>()
                 .InSingletonScope();
 
-            Bind<ICommand>().ToMethod(context =>
+            //Така се подкарва фактори-то, да генерира команди от според типа който подаваме на мястото на GetCommand(null)
+            Bind<ICommand>().ToMethod(context =>            
             {
+                //Кода който се изпълнява, когато се извика метода GetCommand(parameter)
                 Type commandType = (Type)context.Parameters.Single().GetValue(context, null);
                 return (ICommand)context.Kernel.Get(commandType);
+
             }).NamedLikeFactoryMethod((ICommandFactory commandFactory) => commandFactory.GetCommand(null));
+            //Интерфейса, който извикваме.ToFactory()
 
             IConfigurationProvider configurationProvider = Kernel.Get<IConfigurationProvider>();
             if (configurationProvider.IsTestEnvironment)
             {
+                commandFactoryBinding.Intercept().With<StopwatchInterceptor>();
+                studentFactoryBinding.Intercept().With<StopwatchInterceptor>();
+                markFactoryBinding.Intercept().With<StopwatchInterceptor>();
+                //teacherFactoryBinding.Intercept().With<StopwatchInterceptor>();
             }
         }
     }

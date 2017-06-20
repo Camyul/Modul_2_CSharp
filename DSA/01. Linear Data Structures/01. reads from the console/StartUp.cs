af23aa0843a56f09fbf0153e9649fb9ca9f19456
Camyul <vankata.lt@outlook.com>
2017-06-21 02:02:40 +0300

    DSA Linear Data Structures Task2 Complete

---
+++ DSA/01. Linear Data Structures/01. reads from the console/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reads_from_the_console
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            IList<int> sequence = new List<int>();
+
+            while (true)
+            {
+                int number;
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out number))
+                {
+                    sequence.Add(number);
+                }
+                else
+                {
+
+                    if (input == string.Empty)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter correct number!");
+                    }
+                }
+            }
+
+            int sum = 0;
+            foreach (var num in sequence)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ DSA/01. Linear Data Structures/reads N integers/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reads_N_integers
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int count = 3;
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                stack.Push(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}

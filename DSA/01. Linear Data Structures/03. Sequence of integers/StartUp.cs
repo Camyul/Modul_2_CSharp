8b2f4f8b90ba504aa700e958dbdff4f2d229d216
Camyul <vankata.lt@outlook.com>
2017-06-21 10:13:11 +0300

    DSA Linear Data Structures - Task3 Complete

---
+++ DSA/01. Linear Data Structures/03. Sequence of integers/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sequence_of_integers
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<int> sequence = new List<int>();
+
+            while (true)
+            {
+                int number;
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out number))
+                {
+                    sequence.Add(number);
+                }
+                else
+                {
+
+                    if (input == string.Empty)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter correct number!");
+                    }
+                }
+            }
+
+            sequence.Sort();
+
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+    }
+}

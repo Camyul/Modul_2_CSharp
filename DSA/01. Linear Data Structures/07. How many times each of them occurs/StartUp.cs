3a80f975e4d25b61013775398e5a9994f4595fc7
Camyul <vankata.lt@outlook.com>
2017-06-21 21:41:58 +0300

    DSA Linear Data Structures Task8 Complete

--- DSA/01. Linear Data Structures/07. How many times each of them occurs/StartUp.cs
+++ DSA/01. Linear Data Structures/07. How many times each of them occurs/StartUp.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _07.How_many_times_each_of_them_occurs
 {
---
+++ DSA/01. Linear Data Structures/08. FindArrayMajorant/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.FindArrayMajorant
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {
+            int[] sequence = { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
+
+            int? majorant = GetMajorant(sequence);
+
+            if (majorant == null)
+            {
+                Console.WriteLine("Array haven't majorant!");
+            }
+            else
+            {
+                Console.WriteLine($"Array majorant is: {majorant}");
+            }
+        }
+
+        private static int? GetMajorant(int[] sequence)
+        {
+            int minCount = (sequence.Length / 2) +1;
+
+            int? result = null;
+
+            sequence
+                .ToList()
+                .ForEach(x =>
+                {
+                    int count = sequence.ToList().FindAll(num => num == x).Count();
+                    if (count >= minCount)
+                    {
+                        result = x;
+                    }
+                });
+
+            return result;
+        }
+    }
+}

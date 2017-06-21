6f0d277ba9c3f139243eb8a1b437ebf325caeb1c
Camyul <vankata.lt@outlook.com>
2017-06-21 10:48:46 +0300

    DSA Linear Data Structures - Task4 Complete

---
+++ DSA/01. Linear Data Structures/04. Longest subsequence/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_subsequence
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            IList<int> listOfInt = new List<int>() { 2, 3, 3, 1, 5, 5, 5, 2, 2, 1, 1, 1, 1, 5, 4, 4, 4 };
+
+            IList<int> tempLonguestSequence = new List<int>();
+
+            IList<int> longuestSequence = new List<int>();
+
+            for (int i = 0; i < listOfInt.Count - 1; i++)
+            {
+                tempLonguestSequence.Add(listOfInt[i]);
+
+                if (listOfInt[i] != listOfInt[i + 1])
+                {
+                    if (longuestSequence.Count < tempLonguestSequence.Count)
+                    {
+                        longuestSequence.Clear();
+                        longuestSequence = tempLonguestSequence.ToList();
+                    }
+
+                    tempLonguestSequence.Clear();
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", longuestSequence));
+        }
+    }
+}

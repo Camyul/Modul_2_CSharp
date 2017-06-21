3e82e47dacf9ed66139d0f02425906211eeefa29
Camyul <vankata.lt@outlook.com>
2017-06-21 18:48:59 +0300

    DSA Linear Data Structures Task7 Complete

---
+++ DSA/01. Linear Data Structures/06. Removes occur odd number of times/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Removes_occur_odd_number_of_times
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            IList<int> sequence = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+
+            IList<int> listOfRemoved = new List<int>();
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                int item = sequence[i];
+
+                if (IsOdd(item))
+                {
+                    int count = 1;
+
+                    for (int j = 0; j < sequence.Count; j++)
+                    {
+                        if (i == j)
+                        {
+                            continue;
+                        }
+
+                        if (item == sequence[j])
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count > 1)
+                    {
+                        listOfRemoved.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", listOfRemoved));
+        }
+
+        private static bool IsOdd(int item)
+        {
+            if (item % 2 > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DSA/01. Linear Data Structures/07. How many times each of them occurs/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.How_many_times_each_of_them_occurs
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {
+            int[] sequence = { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+
+            sequence
+                .Distinct()
+                .OrderBy(n => n)
+                .ToList()
+                .ForEach(x =>
+                {
+                    int count = sequence.ToList().FindAll(num => num == x).Count();
+                    Console.WriteLine($"{x} - {count} times");
+                });
+        }
+    }
+}

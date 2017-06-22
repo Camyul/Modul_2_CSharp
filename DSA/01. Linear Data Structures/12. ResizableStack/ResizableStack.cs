e614d49ed7c69808fe500bb7a672ea1ea7bfa936
Camyul <vankata.lt@outlook.com>
2017-06-22 19:15:39 +0300

    DSA Linear Data Structures Task12 Complete

---
+++ DSA/01. Linear Data Structures/12. ResizableStack/ResizableStack.cs
@@ -0,0 +1,43 @@
+﻿namespace _12.ResizableStack
+{
+    internal class ResizableStack<T>
+    {
+        private T[] data;
+
+        public ResizableStack()
+        {
+            this.data = new T[4];
+        }
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.data.Length;
+            }
+        }
+
+        public void Push(T value)
+        {
+            if (this.Count == this.data.Length && this.Count > 0)
+            {
+                T[] newArray = new T[this.Capacity * 2];
+                this.data.CopyTo(newArray, 0);
+                this.data = newArray;
+            }
+
+            this.data[this.Count] = value;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            T result = this.data[this.Count - 1];
+            this.Count--;
+
+            return result;
+        }
+    }
+}
---
+++ DSA/01. Linear Data Structures/12. ResizableStack/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12.ResizableStack
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {
+            ResizableStack<int> stack = new ResizableStack<int>();
+            int interactions = 17;
+
+            for (int i = 0; i < interactions; i++)
+            {
+                stack.Push(i);
+                Console.WriteLine($"Added '{i}' to stack! --- Count: {stack.Count} --- Capacity: {stack.Capacity}");
+            }
+
+            Console.WriteLine(new string('-', 64));
+
+            for (int i = 0; i < interactions; i++)
+            {
+                int poped = stack.Pop();
+                Console.WriteLine($"Poped element ot stack with value: {poped} --- Stack count: {stack.Count}");
+            }
+        }
+    }
+}

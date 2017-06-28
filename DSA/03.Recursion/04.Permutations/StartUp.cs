fb81fe4ff39e3f045036b974477404b55d00eb54
Camyul <vankata.lt@outlook.com>
2017-06-28 23:36:37 +0300

    DSA Linear Racursion Task 4 - Complete

---
+++ DSA/03.Recursion/04.Permutations/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Permutations
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            GeneratePermutations(0, arr);
+        }
+
+        private static void GeneratePermutations<T>(int k, T[] arr)
+        {
+            if (k >= arr.Length)
+            {
+                Print(arr);
+            }
+            else
+            {
+                GeneratePermutations(k + 1, arr);
+                for (int i = k + 1; i < arr.Length; i++)
+                {
+                    Swap(ref arr[k], ref arr[i]);
+                    GeneratePermutations(k + 1, arr);
+                    Swap(ref arr[k], ref arr[i]);
+                }
+            }
+
+        }
+
+        private static void Swap<T>(ref T first, ref T second)
+        {
+            T temp = first;
+            first = second;
+            second = temp;
+        }
+
+        private static void Print<T>(T[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}

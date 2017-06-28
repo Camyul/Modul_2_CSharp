3bf65fbab450d95cb38f79ce0b28144087a9f07c
Camyul <vankata.lt@outlook.com>
2017-06-29 01:28:22 +0300

    DSA Linear Racursion Task 5 - Complete

---
+++ DSA/03.Recursion/05.NKSubsets/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05.NKSubsets
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("K = ");
+            int k = int.Parse(Console.ReadLine());
+            int[] numbers = new int[k];
+            Console.Write("Set: ");
+            string[] separator = { " " };
+            string[] set = Console.ReadLine().Split(separator, StringSplitOptions.None);
+            string[] arr = new string[k];
+
+            GenerateVariations(k, 0, set, arr);
+
+        }
+
+        private static void GenerateVariations<T>(int k, int index, T[] set, T[] arr)
+        {
+            if (index >= k)
+            {
+                Print(arr);
+            }
+            else
+            {
+                for (int i = 0; i < set.Length; i++)
+                {
+                    arr[index] = set[i];
+                    GenerateVariations(k, index + 1, set, arr);
+                }
+            }
+        }
+
+        private static void Print<T>(T[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}

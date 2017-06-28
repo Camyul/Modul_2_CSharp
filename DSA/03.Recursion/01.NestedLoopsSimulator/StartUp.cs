22949268cf5cc782f2abe3d5ea81fa4b9c110570
Camyul <vankata.lt@outlook.com>
2017-06-28 22:21:37 +0300

    DSA Linear Racursion Task 2 - Complete

---
+++ DSA/03.Recursion/01.NestedLoopsSimulator/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.NestedLoopsSimulator
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {            
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+
+            Recursion(0, numbers);
+        }
+
+        private static void Recursion(int index, int[] numbers)
+        {
+            if (index >= numbers.Length)
+            {
+                PrintNumbers(numbers);
+                //return;
+            }
+            else
+            {
+                for (int i = 1; i <= numbers.Length; i++)
+                {
+                    numbers[index] = i;
+                    Recursion(index + 1, numbers);
+                }
+            }
+        }
+
+        private static void PrintNumbers(int[] numbers)
+        {
+             Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
---
+++ DSA/03.Recursion/02.CombinationsWithDuplications/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CombinationsWithDuplications
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("K = ");
+            int k = int.Parse(Console.ReadLine());
+            int[] numbers = new int[k];
+            Recursion(n, 0, 0, numbers);
+
+
+        }
+
+        private static void Recursion(int n, int index, int start, int[] numbers)
+        {
+            if (index == numbers.Length)
+            {
+                PrintNumber(numbers);
+                return;
+            }
+
+            for (int i = start; i <= numbers.Length; i++)
+            {
+                numbers[index] = i + 1;
+                Recursion(n, index + 1, i, numbers);
+            }
+        }
+
+        private static void PrintNumber(int[] numbers)
+        {
+            Console.WriteLine($"({string.Join(" ", numbers)})");
+        }
+    }
+}

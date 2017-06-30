dc0d04a97cadc3bde237ed4f59e37fd1cdb540e3
Camyul <vankata.lt@outlook.com>
2017-06-30 19:12:03 +0300

    DSA Searching Algoritm Binnary Search mplemented

---
+++ DSA/06. Searching-Algorithms/SortingHomework/SortableCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingHomework
+{
+    public class SortableCollection<T> where T : IComparable<T>
+    {
+        private readonly IList<T> collection;
+
+        public SortableCollection(IEnumerable<T> collection)
+        {
+            this.collection = new List<T>(collection);
+        }
+
+        public IList<T> Collection
+        {
+            get
+            {
+                return this.collection;
+            }
+        }
+
+        public int BinarySearch(T item, int left, int right)
+        {
+
+            while (left < right)
+            {
+                int middle = (left + right) / 2;
+                int cmp = this.Collection[middle].CompareTo(item);
+
+                if (cmp < 0)
+                {
+                    left = middle + 1;
+                }
+                else if (cmp > 0)
+                {
+                    right = middle;
+                }
+                else
+                {
+                    return middle;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ DSA/06. Searching-Algorithms/SortingHomework/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace SortingHomework
+{
+    internal static class StartUp
+    {
+        static void Main()
+        {
+            var collection = new SortableCollection<int>(new[] { 0, 11, 22, 33, 33, 33, 101, 101 });
+            int searchedIndex = collection.BinarySearch(33, 0, collection.Collection.Count);
+            System.Console.WriteLine(searchedIndex);
+        }
+    }
+}

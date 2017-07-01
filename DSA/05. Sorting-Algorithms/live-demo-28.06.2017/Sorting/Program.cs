b90ba9670ce397052b48fee69393bbe52a66615e
Camyul <vankata.lt@outlook.com>
2017-07-01 11:01:22 +0300

    DSA Sorting Algoritm Add Live Demo

---
+++ DSA/05. Sorting-Algorithms/live-demo-28.06.2017/Sorting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE = 10000000;
+            var array = new int[SIZE];
+
+            var rnd = new Random();
+            for (int i = 0; i < SIZE; ++i)
+            {
+                array[i] = rnd.Next() % 1000;
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            array.RadixRightToLeft(2, 65536);
+            //array.MergeSortIterative();
+            stopwatch.Stop();
+            //Console.WriteLine(String.Join(" ", array));
+            Console.WriteLine(stopwatch.Elapsed);
+
+
+
+            //Console.WriteLine(string.Join(" ", array));
+            for (int i = 1; i < array.Length; ++i)
+            {
+                if(array[i - 1] > array[i])
+                {
+                    Console.WriteLine("Error");
+                    break;
+                }
+            }
+        }
+    }
+}

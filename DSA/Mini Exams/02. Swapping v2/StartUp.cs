8eb8fd47aeb3867e72dd4f17317d56ebc9b6b4c6
Camyul <vankata.lt@outlook.com>
2017-07-05 13:21:33 +0300

    DSA Aded Mini Exam 1 some

---
+++ DSA/Mini Exams/02. Swapping v2/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Swapping_v2
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = Enumerable.Range(1, n).ToList();
+
+            var swapNumbers = Console.ReadLine()
+                         .Split()
+                         .Select(int.Parse)
+                         .ToArray();
+
+            foreach (var num in swapNumbers)
+            {
+                int index = numbers.IndexOf(num);
+
+            }
+
+        }
+    }
+}

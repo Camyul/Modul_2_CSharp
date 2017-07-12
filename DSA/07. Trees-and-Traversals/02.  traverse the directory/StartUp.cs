0f55d144c7516f60481d967396e52597189494be
Camyul <vankata.lt@outlook.com>
2017-07-12 19:01:58 +0300

    DSA Trees and Traversals - Task-2 Complete

---
+++ DSA/07. Trees-and-Traversals/02.  traverse the directory/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _02.traverse_the_directory
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var folderPath = "F:\\games";
+            List<string> contents = new List<string>();
+            GetFilesFromDirectory(folderPath, contents);
+
+            Console.WriteLine(string.Join("\n", contents));
+        }
+
+        private static void GetFilesFromDirectory(string folderPath, List<string> contents)
+        {
+
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.exe"))
+            {
+                contents.Add(file.ToLower());
+            }
+
+
+            foreach (var dir in Directory.EnumerateDirectories(folderPath))
+            {
+                folderPath = dir;
+                GetFilesFromDirectory(folderPath, contents);
+            }
+        }
+    }
+}

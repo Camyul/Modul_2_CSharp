6cfd9b59f0c817d09d80a760483879993113a531
Camyul <vankata.lt@outlook.com>
2017-07-15 20:11:09 +0300

    DSA Mini exam 15.07.2017

---
+++ DSA/Mini Exams/MiniExam-15.07.17/01. Binary tree/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Binary_tree
+{
+    public class StartUp
+    {
+        public static int p;
+        public static bool isPretty = false;
+        public static List<int> isPrettyResults = new List<int>();
+
+        public class Node
+        {
+            public Node(double value)
+            {
+                this.Value = value;
+            }
+
+            public double Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+
+        }
+
+        public class Tree
+        {
+            public void Traverse(Node node, long searchedNumber)
+            {
+                if (node.Value == searchedNumber)
+                {
+                    isPretty = true;
+                    return;
+                }
+                if (node == null || node.Value > searchedNumber)
+                {
+                    return;
+                }
+
+                node.Left = new Node(node.Value * p);
+                node.Right = new Node((node.Value * p) + 1);
+
+                this.Traverse(node.Left, searchedNumber);
+                this.Traverse(node.Right, searchedNumber);
+            }
+        }
+
+        static void Main()
+        {
+            p = int.Parse(Console.ReadLine());
+
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Node root = new Node(1);
+
+            Tree tree = new Tree();
+
+            foreach (var number in numbers)
+            {
+                tree.Traverse(root, number);
+                if (isPretty)
+                {
+                    isPrettyResults.Add(1);
+                }
+                else
+                {
+                    isPrettyResults.Add(0);
+                }
+                isPretty = false;
+            }
+
+            Console.WriteLine(string.Join(" ", isPrettyResults));
+        }
+    }
+}
---
+++ DSA/Mini Exams/MiniExam-15.07.17/02. Bike/StartUp.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bike
+{
+    class StartUp
+    {
+        public class DijkstraWithoutQueue
+        {
+            private static int rows;
+            private static int cols;
+            private static readonly double[] Distance = new double[500];
+            private static readonly double?[] Previous = new double?[500];
+            private static readonly HashSet<int> SetOfNodes = new HashSet<int>();
+
+            public static void Dijkstra(double[,] graph, int source)
+            {
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    Distance[i] = int.MaxValue;
+                    Previous[i] = null;
+                    SetOfNodes.Add(i);
+                }
+
+                Distance[source] = 0;
+
+                while (SetOfNodes.Count != 0)
+                {
+                    int minNode = int.MaxValue;
+
+                    foreach (var node in SetOfNodes)
+                    {
+                        if (minNode > Distance[node])
+                        {
+                            minNode = node;
+                        }
+                    }
+
+                    SetOfNodes.Remove(minNode);
+
+                    if (minNode == int.MaxValue)
+                    {
+                        break;
+                    }
+
+                    for (int i = 0; i < rows; i++)
+                    {
+                        if (graph[minNode, i] > 0)
+                        {
+                            double potentialDistance = Distance[minNode] + graph[minNode, i];
+                            if (potentialDistance < Distance[i])
+                            {
+                                Distance[i] = potentialDistance;
+                                Previous[i] = minNode;
+                            }
+                        }
+                    }
+                }
+            }
+
+            public static void Main()
+            {
+                rows = int.Parse(Console.ReadLine());
+
+                cols = int.Parse(Console.ReadLine());
+
+                double[,] graph = new double[rows, cols];
+
+                for (int i = 0; i < rows; i++)
+                {
+                    double[] row = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+                    for (int j = 0; j < cols; j++)
+                    {
+                        graph[i, j] = row[j];
+                    }
+                }
+
+                int source = 1;
+
+                Dijkstra(graph, source - 1);
+                Console.WriteLine(string.Join(",", Distance));
+
+                //for (int i = 0; i < Distance.Length; i++)
+                //{
+                //    Console.Write("Distance from 1 to {0}: ", i + 1);
+                //    if (Distance[i] == int.MaxValue)
+                //    {
+                //        Console.WriteLine("No path.");
+                //    }
+                //    else
+                //    {
+                //        Console.Write("Path: ");
+
+                //        int? indexer = i;
+                //        while (indexer != 0)
+                //        {
+                //            Console.Write(indexer + 1 + " ");
+                //            indexer = (int)Previous[indexer.Value];
+                //        }
+
+                //        Console.Write("1 ");
+
+                //        Console.WriteLine("Distance: " + Distance[i]);
+                //    }
+                //}
+            }
+        }
+    }
+}
+/*
+4
+4
+1 8 9 6
+1 4,3 2,7 -3
+1 -64 3 3
+1 1 1 1
+ */

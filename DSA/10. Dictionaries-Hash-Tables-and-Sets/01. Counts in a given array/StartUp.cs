14c7f6b56e2167bf3505ded155b1063963327abb
Camyul <vankata.lt@outlook.com>
2017-07-15 12:05:15 +0300

    DSA Dictionary HashSet Task2 - Complete

---
+++ DSA/10. Dictionaries-Hash-Tables-and-Sets/01. Counts in a given array/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Counts_in_a_given_array
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            double[] array = new double[] { 3, 4, 4, -2.5, 3, 3, 4, 3, -2.5 };
+
+            // Dictionary<double, int> dictionary = new Dictionary<double, int>();
+            SortedDictionary<double, int> dictionary = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dictionary.ContainsKey(array[i]))
+                {
+                    dictionary[array[i]] += 1;
+                }
+                else
+                {
+                    dictionary.Add(array[i], 1);
+                }
+            }
+
+            foreach (var num in dictionary)
+            {
+                Console.WriteLine("{0} -> {1}", num.Key, num.Value);
+            }
+        }
+    }
+}
---
+++ DSA/10. Dictionaries-Hash-Tables-and-Sets/02. Extracts from a given sequence odd number/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Extracts_from_a_given_sequence_odd_number
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] array = new string[] { "C#", "SQL", "PHP", "PHP", "SQL", "SQL" };
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            // SortedDictionary<double, int> dictionary = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dictionary.ContainsKey(array[i]))
+                {
+                    dictionary[array[i]] += 1;
+                }
+                else
+                {
+                    dictionary.Add(array[i], 1);
+                }
+            }
+
+            Console.Write("{ ");
+            foreach (var num in dictionary)
+            {
+                if (num.Value % 2 > 0)
+                {
+                    Console.Write("{0}, ", num.Key);
+                }
+            }
+            Console.Write(" }");
+        }
+    }
+}

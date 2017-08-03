2d52851793833ac6a44895ef7796040f102358ef
Camyul <vankata.lt@outlook.com>
2017-08-03 19:18:54 +0300

    DSA Strings Algoritm AhoCorasick

---
+++ DSA/StringSearching/AhoCorasick/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhoCorasick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new String[]
+            {
+                "a",
+                "ab",
+                "bab",
+                "bc",
+                "bca",
+                "c",
+                "caa"
+            };
+           
+            var root = new Trie();
+            foreach (var str in strings)
+            {
+                root.AddString(str);
+            }
+
+            root.Precompute();
+            // trie.Dfs();
+
+            var text = "asdbbasbcbascbabcbabcsabc";
+            Console.WriteLine(text);
+            root.AhoCorasick(text);
+        }
+    }
+}

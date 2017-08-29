9f7160bbef1c78f434f7ee8ca4f609fe74ec6591
Camyul <vankata.lt@outlook.com>
2017-08-30 00:16:02 +0300

    Workshop Bussinesman Solved

---
+++ DSA/Workshop/04. Businessmen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Businessmen
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long[] solutions = new long[71];
+            solutions[0] = 1;
+
+            for (int allPeople = 2; allPeople <= n; allPeople += 2)
+            {
+                for (int oneSidePeople = allPeople - 2; oneSidePeople >= 0; oneSidePeople -= 2)
+                {
+                    solutions[allPeople] += solutions[oneSidePeople] * solutions[allPeople - oneSidePeople - 2];
+                }
+            }
+
+            Console.WriteLine(solutions[n]);
+        }
+    }
+}

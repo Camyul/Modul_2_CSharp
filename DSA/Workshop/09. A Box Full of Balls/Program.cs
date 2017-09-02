e60b99ab2a074828f80c04710f1c0b8436538ab9
Camyul <vankata.lt@outlook.com>
2017-09-02 16:14:34 +0300

    Workshop Office Space 92/100

---
+++ DSA/Workshop/09. A Box Full of Balls/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.A_Box_Full_of_Balls
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[] ab = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int a = ab[0];
+            int b = ab[1];
+
+            
+            var isWins = new bool[b + 1];
+            isWins[0] = false;
+            for (int i = 1; i <= b; i++)
+            {
+                foreach (var move in moves)
+                {
+                    if (move > i)
+                    {
+                        continue;
+                    }
+                    if (!isWins[i - move])
+                    {
+                        isWins[i] = true;
+                    }
+                }
+            }
+
+            int total = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (isWins[i])
+                {
+                    total++;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ DSA/Workshop/10. Office Space - Recursive/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Office_Space___Recursive
+{
+    class Program
+    {                   // 92/100
+        static int[] answers = new int[50];
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var minutes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            List<int>[] dependencies = new List<int>[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                dependencies[i] = Console.ReadLine().Split(' ').Select(x => int.Parse(x) - 1).ToList();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                answers[i] = CalcMinTime(i, minutes, dependencies);
+            }
+
+            Console.WriteLine(answers.Max());
+        }
+
+        static int CalcMinTime(int taskId, int[] minutes, List<int>[] dependencies)
+        {
+            if (answers[taskId] != 0)
+            {
+                return answers[taskId];
+            }
+
+            if (dependencies[taskId].Count == 1 && dependencies[taskId][0] == -1)
+            {
+                return minutes[taskId];
+            }
+
+            int maxDependecyTime = 0;
+            foreach (int dependencyId in dependencies[taskId])
+            {
+                var dependecyTime = CalcMinTime(dependencyId, minutes, dependencies);
+
+                maxDependecyTime = Math.Max(dependecyTime, maxDependecyTime);
+            }
+
+            answers[taskId] = minutes[taskId] + maxDependecyTime;
+
+            return answers[taskId];
+        }
+    }
+}
